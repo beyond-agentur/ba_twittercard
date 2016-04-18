d15cdc991ea6b36fc61434b91ad9dbe21c311dc5
Kersten Burkhardt <kerstenk@gmail.com>
2016-04-18 15:45:03 +0200

    Added first stuff

---
+++ ext_emconf.php
@@ -0,0 +1,29 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "ba_twittercard".
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'Twitter Crads',
+	'description' => 'This Extension adds twitter cards properties in meta-tags to the html-header and supports multilanguage-websites.',
+	'category' => 'plugin',
+	'version' => '0.0.1',
+	'state' => 'stable',
+	'uploadfolder' => false,
+	'createDirs' => '',
+	'clearcacheonload' => true,
+	'author' => 'beyond Agentur UG',
+	'author_email' => 'info@beyond-agentur.com',
+	'author_company' => 'beyond Agentur UG',
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+			'typo3' => '7.6.0-8.0.99',
+		),
+		'suggests' => 
+		array (
+		),
+	),
+);
---
+++ ext_tables.sql
@@ -0,0 +1,21 @@
+#
+# Table structure for table 'pages'
+#
+CREATE TABLE pages (
+	tx_batwittercard_title tinytext,
+	tx_batwittercard_type tinytext,
+	tx_batwittercard_image text,
+	tx_batwittercard_falimages int(11) unsigned DEFAULT '0',
+	tx_batwittercard_description tinytext
+);
+
+#
+# Table structure for table 'pages_language_overlay'
+#
+CREATE TABLE pages_language_overlay (
+	tx_batwittercard_title tinytext,
+	tx_batwittercard_type tinytext,
+	tx_batwittercard_image text,
+	tx_batwittercard_falimages int(11) unsigned DEFAULT '0',
+	tx_batwittercard_description tinytext
+);
