bb8852662231f8c91b7472ec1858a75664417091
Kersten Burkhardt <kerstenk@gmail.com>
2016-04-19 16:06:00 +0200

    copy from JhOpengraph

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,7 +3,8 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-$TYPO3_CONF_VARS['FE']['pageOverlayFields'] .= ',tx_jhopengraphprotocol_ogtitle,tx_jhopengraphprotocol_ogtype,tx_jhopengraphprotocol_ogfalimages,tx_jhopengraphprotocol_ogdescription';
+$TYPO3_CONF_VARS['FE']['pageOverlayFields'] .= ',tx_batwittercard_title,tx_batwittercard_type,tx_batwittercard_falimages,tx_batwittercard_description';
 
 // Add update script to install tool
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['jh_opengraphprotocol_fal'] = 'Heilmann\\JhOpengraphprotocol\\Updates\\FalUpdateWizard';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['ba_twittercard_fal'] =
+    'BeyondAgentur\\Twittercard\\Updates\\FalUpdateWizard';
