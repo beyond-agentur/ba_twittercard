7b44146fd313d9f6e461f79d0438a8884f57ec62
Kersten Burkhardt <kerstenk@gmail.com>
2016-04-20 11:18:32 +0200

    Added PageRenderer Hook

---
+++ Classes/Hooks/Frontend/PageRenderer.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace BeyondAgentur\Twittercard\Hooks\Frontend;
+
+class PageRenderer
+{
+	public function execute()
+	{
+		print_r( 'FICKEN' );
+		exit();
+	}
+}
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,10 +1,19 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
+
+use TYPO3\CMS\Core\Page\PageRenderer;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+if ( !defined( 'TYPO3_MODE' ) ) {
+	die( 'Access denied.' );
 }
 
-$TYPO3_CONF_VARS['FE']['pageOverlayFields'] .= ',tx_batwittercard_title,tx_batwittercard_type,tx_batwittercard_falimages,tx_batwittercard_description';
+$TYPO3_CONF_VARS[ 'FE' ][ 'pageOverlayFields' ] .= ',tx_batwittercard_type,tx_batwittercard_title,tx_batwittercard_description,tx_batwittercard_falimages';
+
+if (TYPO3_MODE === 'FE') {
+	$GLOBALS[ 'TYPO3_CONF_VARS' ][ 'SC_OPTIONS' ][ 't3lib/class.t3lib_pagerenderer.php' ][ 'render-preProcess'
+	]['tx_batwittercard'] = 'BeyondAgentur\Twittercard\Hooks\Frontend\PageRenderer->execute';
+}
 
 // Add update script to install tool
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['ba_twittercard_fal'] =
-    'BeyondAgentur\\Twittercard\\Updates\\FalUpdateWizard';
+$GLOBALS[ 'TYPO3_CONF_VARS' ][ 'SC_OPTIONS' ][ 'ext/install' ][ 'update' ][ 'ba_twittercard_fal' ] =
+	'BeyondAgentur\\Twittercard\\Updates\\FalUpdateWizard';
